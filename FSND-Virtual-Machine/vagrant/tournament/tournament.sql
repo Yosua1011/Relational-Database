f28201d5f7aad615ffc6f1b9bd739f5528096023
Yosua Tresno Surojo <yosua1011@gmail.com>
2016-12-18 22:29:06 +0700

    Tournament Project Init
    Start working with Swiss Tournament Project

--- FSND-Virtual-Machine/vagrant/tournament/tournament.sql
+++ FSND-Virtual-Machine/vagrant/tournament/tournament.sql
@@ -5,5 +5,9 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+\c tournament;
 
+CREATE TABLE PLAYERS (id serial PRIMARY KEY, name varchar(50) UNIQUE NOT NULL);
+CREATE TABLE MATCHES (player integer references PLAYERS(id), rounds integer NOT NULL, match integer NOT NULL );
 
+DROP TABLE IF EXISTS PLAYERS, MATCHES RESTRICT;
